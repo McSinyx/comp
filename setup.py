f5ea68509e606011630e1d8768a3ea86cddd68bb
Raphael McSinyx <vn.mcsinyx@gmail.com>
2017-03-20 20:49:11 +0700

    Initial comp commit

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
-from setuptools import setup, find_packages
-setup(
-    name = 'mpv',
-    version = '0.1',
-    py_modules = ['mpv'],
-    description = ('A python interface to the mpv media player'),
-    url = 'https://github.com/jaseg/python-mpv',
-    author = 'jaseg',
-    author_email = 'github@jaseg.net',
-    license = 'AGPLv2'
+from distutils.core import setup
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(name = 'comp',
+      version = '0.1.0a1',
+      url = 'https://github.com/McSinyx/comp',
+      description = ('console/curses online media player'),
+      long_description=long_description,
+      author = 'McSinyx',
+      author_email = 'vn.mcsinyx@gmail.com',
+      license = 'AGPLv2',
+      py_modules = ['mpv'],
+      scripts=['comp']
 )
