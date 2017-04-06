6a2c20e4901bf91749fc138a9494752644e4c3b4
Raphael McSinyx <vn.mcsinyx@gmail.com>
2017-04-06 23:02:54 +0700

    Add save playlist function and complete Vietnamese translation

--- setup.py
+++ setup.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python3
 
 from distutils.core import setup
-from os.path import expanduser
+from sys import prefix
 
 with open('README.rst') as f:
     long_description = f.read()
 
-setup(name = 'comp', version = '0.1.1a1',
-      url = 'https://github.com/McSinyx/comp',
-      description = ('Curses Online Media Player'),
+setup(name='comp', version='0.1.1a2',
+      url='https://github.com/McSinyx/comp',
+      description=('Curses Online Media Player'),
       long_description=long_description,
-      author = 'McSinyx', author_email = 'vn.mcsinyx@gmail.com',
-      py_modules = ['mpv'], scripts=['comp'],
-      data_files=[('/etc/comp', ['settings.ini'])],
-      classifiers = [
+      author='Nguyễn Gia Phong', author_email='vn.mcsinyx@gmail.com',
+      py_modules=['mpv'], scripts=['comp'],
+      data_files=[
+          ('{}/share/locale/vi/LC_MESSAGES/'.format(prefix), ['locale/vi/LC_MESSAGES/comp.mo']),
+          ('/etc/comp', ['settings.ini'])
+      ], classifiers=[
           'Development Status :: 3 - Alpha',
           'Environment :: Console :: Curses',
           'Intended Audience :: End Users/Desktop',
           'License :: OSI Approved :: GNU Affero General Public License v3',
           'Natural Language :: English',
-          'Natural Language :: Vietnamese',  # planned
+          'Natural Language :: Vietnamese',
           'Operating System :: POSIX',
           'Programming Language :: Python :: 3.5',
           'Topic :: Multimedia :: Sound/Audio :: Players',
           'Topic :: Multimedia :: Video :: Display'
-      ], license = 'AGPLv3')
+      ], platforms=['POSIX'], license='AGPLv3')
