5be79fd930bbaa33a4956e70c567b99efbae2b19
Raphael McSinyx <vn.mcsinyx@gmail.com>
2017-05-28 21:02:34 +0700

    Begin to create an independant back-end object

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
 from os import walk
 from os.path import join
 from sys import prefix
+
+from setuptools import setup
 
 with open('README.rst') as f:
     long_description = f.read()
 
 setup(
     name='comp',
-    version='0.2.2',
+    version='0.3.0',
     description=('Curses Online Media Player'),
     long_description=long_description,
     url='https://github.com/McSinyx/comp',
     author='Nguyễn Gia Phong',
     author_email='vn.mcsinyx@gmail.com',
-    license='AGPLv3',
+    license='AGPLv3+',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console :: Curses',
@@ -31,13 +32,13 @@ setup(
         'Topic :: Multimedia :: Video :: Display'
     ],
     keywords='youtube-dl mpv-wrapper curses console-application multimedia',
-    install_requires=['youtube-dl'],
+    install_requires=['python-mpv', 'youtube-dl'],
     data_files=[
         *((join(prefix, 'share', i[0]), [join(i[0], 'comp.mo')])
           for i in walk('locale') if i[2]),
         ('/etc/comp', ['settings.ini'])
     ],
-    py_modules=['mpv'],
+    py_modules=['omp'],
     scripts=['comp'],
     platforms=['POSIX']
 )
