be41f8a645f66fee4ad7bd59906ec010762fc80e
Raphael McSinyx <vn.mcsinyx@gmail.com>
2017-09-13 21:08:12 +0700

    Start implementing default mpv keybindings

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as f:
 
 setup(
     name='comp',
-    version='0.3.12',
+    version='0.4.0',
     description=('Curses Omni Media Player'),
     long_description=long_description,
     url='https://github.com/McSinyx/comp',
